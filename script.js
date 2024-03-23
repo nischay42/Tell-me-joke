a7e6cda1b65ede02d72855693608379d60d119ce
nischay42 <155072804+nischay42@users.noreply.github.com>
2024-03-23 23:36:37 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,107 @@
+let jokes =  [
+    "What did the horse say after it tripped? Help! I've fallen and I can't giddyup!",
+    "Why can't you hear a pterodactyl going to the bathroom? Because the “P” is silent.",
+    "What do you call a well-balanced horse? Stable.",
+    "What do you call an angry carrot? A steamed veggie.",
+    "Where do polar bears keep their money? In a snowbank.",
+    "How do you make an egg-roll? You push it!",
+    "What would bears be without bees? Ears.",
+    "What do you call a pile of cats? A meow-ntain.",
+    "Why do cows wear bells? Because their horns don't work.",
+    "Why did the bicycle fall over? Because it was two tired.",
+    "What did the triangle say to the circle? You're pointless.",
+    "RIP, boiling water. You will be mist.",
+    "Time flies like an arrow. Fruit flies like a banana.",
+    "I ordered a chicken and an egg online. I'll let you know what comes first.",
+    "Why was Cinderella so bad at soccer? She kept running away from the ball!",
+    "What do lawyers wear to court? Lawsuits.",
+    "What do elves learn in school? The elf-abet.",
+    "Where was King David's temple located? Beside his ear.",
+    "What did one toilet say to another? You look flushed.",
+    "What lights up a soccer stadium? A soccer match.",
+    "What does corn say when it gets a compliment? Aw, shucks!",
+    "What's the difference between a poorly dressed man on a tricycle and a well-dressed man on a bicycle? Attire.",
+    "What's red and bad for your teeth? A brick.",
+    "What do sprinters eat before they race? Nothing. They fast.",
+    "What has more lives than a cat? A frog, because it croaks every day.",
+    "Why was the fish's grades bad? They were below sea level.",
+    "What's Forrest Gump's password? 1forrest1.",
+    "What do you call a fish without an eye? Fsh.",
+    "Why shouldn't you use a broken pencil? Because it's pointless.",
+    "What do you call a pig that practices karate? A pork chop.",
+    "What has four wheels and flies? A garbage truck!",
+    "What did the tomato say to the other tomato during a race? Ketchup.",
+    "Why shouldn't you write with a broken pen? Because it's pointless.",
+    "Why can't you trust the king of the jungle? Because he's always lion.",
+    "What did one wall say to the other? \"I'll meet you at the corner.\"",
+    "What do sea monsters eat? Fish and ships.",
+    "What do you call a sad strawberry? A blue berry!",
+    "Why are pirates called pirates? They just ARRRR!",
+    "How do you organize a space party? You planet.",
+    "Why do seagulls fly over the sea? If they flew over the bay, they would be bagels.",
+    "What do cows read the most? Cattle-logs.",
+    "I lost an electron. You really have to keep an ion them!",
+    "What do clouds wear under their shorts? Thunder pants!",
+    "How many tickles does it take to make an octopus laugh? Ten-tickles.",
+    "What did 0 say to 8? \"Nice belt.\"",
+    "How did the hipster burn his tongue? He drank his coffee before it was cool.",
+    "What did the drummer name her twin daughters? Anna 1, Anna 2.",
+    "What did the lettuce say to the celery? Quit stalking me!",
+    "What's small and red and has a rough voice? A hoarse raddish!",
+    "Why do mushrooms get invited to all the parties? Because they are such fungis."
+    , "Knock knock. Who's there? Figs. Figs who? Figs the doorbell. I've been knocking forever!"
+, "Knock knock. Who's there? To. To who? No, it's to whom!"
+, "Knock knock. Who's there? Spell. Spell who? W.H.O."
+, "Knock knock. Who's there? Cow says. Cow says who? No, a cow says moo."
+, "Knock knock. Who's there? Etch. Etch who? Bless you!"
+, "Knock knock. Who's there? Tank. Tank Who? You're welcome!"
+, "Knock knock. Who's there? Boo. Boo who? Don't cry, I'm only joking!"
+, "Knock knock. Who's there? Norma Lee. Norma Lee who? Normally I ring the doorbell."
+, "Knock knock. Who's there? Annie. Annie who? Is Annie body home?"
+, "Knock, knock. Who's there? Wa. Wa who? What are you so excited about?!"
+, "Knock knock. Who's there? Lettuce. Lettuce who? Lettuce in."
+, "Knock, knock. Who's there? Leon. Leon who? Leon me when you're not strong!"
+, "Knock knock. Who's there? Adore. Adore who? Adore is between us, so please open up!"
+, "Knock knock. Who's there? A little old lady. A little old lady who? Wow, I didn't know you could yodel!"
+, "Knock, knock. Who's there? Lena. Lena who? Lena a little closer, and I'll tell you another joke!"
+, "Knock knock. Who's there? Candice. Candice who? Candice joke get any worse"
+, "Knock, knock. Who's there? Goliath. Goliath who? Goliath down, you look-eth tired!"
+, "Knock, knock. Who's there? Broccoli? Broccoli who? Broccoli doesn't have a last name, silly."
+, "Knock, knock. Who's there? Wooden shoe. Wooden shoe who? Wooden shoe like to hear another joke?"
+, "Knock knock. Who's there? Jimmy. Jimmy who? Jimmy crack corn and I don't care!"
+, "Knock, knock. Who's there? Cows go. Cows go who? No silly, cows go MOO!"
+, "Knock, knock. Who's there? Harry. Harry who? Harry up and answer the door!"
+, "Knock, knock. Who's there? Cash. Cash who? No thanks, but I'll take a peanut if you have one!"
+, "Knock knock. Who's there? Honeydew. Honeydew who? Honeydew you wanna dance?"
+, "Knock knock. Who's there? Juno. Juno who? Juno how funny this is?"
+, "Knock knock. Who's there? Weekend. Weekend who? Weekend do anything we want."
+, "Knock knock. Who's there? Watts. Watts who? Watts for dinner? I'm hungry."
+, "If a child refuses to sleep during nap time, are they guilty of resisting a rest?"
+, "If you need help building an ark, I Noah guy."
+, "What did one wall say to the other wall? I'll meet you at the corner!"
+, "Whoever invented knock-knock jokes deserves a no-bell prize."
+, "Don't spell part backward. It's a trap."
+, "Do you know how scientists freshen their breath? With experi-mints!"
+, "Two burglars stole a calendar last night, and they each got six months."
+, "Would a cardboard belt be a waist of paper?"
+, "I always wanted to be a doctor, but I didn't have the patients."
+, "I'm on a seafood diet: When I see food, I eat it."
+, "If at first you don't succeed, then skydiving isn't for you."
+, "I was going to tell a pizza joke, but it was too cheesy."
+, "I just flew into town and my arms are so tired."
+, "I tried to do my homework but my pencil broke, so it was pointless."
+, "I used to be addicted to the Hokey Pokey, but I turned myself around."
+, "The more this towel dries, the wetter it gets."
+, "I ate a clock yesterday, it was very time-consuming."
+, "I've just written a song about tortillas; actually, it's more of a w-rap."
+, "What is black and white and red all over? A zebra with a sunburn."
+, "Which animal makes the best pet? A cat, because it's purr-fect."
+, "What do you get from a pampered cow? Spoiled milk!"
+, "What was the first animal in space? The cow who jumped over the moon."
+];
+
+function nextJoke(){
+let randomJoke = Math.floor(Math.random() * jokes.length);
+  let element = document.getElementById("joke");
+  element.innerHTML=jokes[randomJoke];
+}
